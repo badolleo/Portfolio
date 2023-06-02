0494635ac317d70f44824bce71d81f05bd18072a
Badol leo <113773274+badolleo@users.noreply.github.com>
2023-06-02 09:01:43 +0200

    Delete main directory

--- main/projet1.js
+++
@@ -1,7 +0,0 @@
-const viewMoreButton = document.getElementById("button1");
-const elementAAfficher = document.getElementById("projet1_retex")
-viewMoreButton.addEventListener("click", () => {
-    
-    elementAAfficher.classList.toggle("hidden");
-    
-});
